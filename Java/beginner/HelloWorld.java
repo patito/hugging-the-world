7705d622eb4d0afaf71c9a0ad3a81299ab360055
patito <benatto@gmail.com>
2014-04-25 11:58:06 +0100

    Java Hello World

---
+++ Java/beginner/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Java Hello World - 25/04/2014
+ *
+ * Compile: ~$ javac HelloWorld.java
+            ~$ java HelloWorld
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
