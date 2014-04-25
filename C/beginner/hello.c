72e67db2cc58eacd02b4ff65f36e816fea451070
patito <benatto@gmail.com>
2014-04-25 11:11:02 +0100

    Added Hello World program.

---
+++ C/beginner/hello.c
@@ -0,0 +1,13 @@
+/*
+ * HelloWorld - 25/04/2014
+ * 
+ * Compile: gcc hello.c -o hello -Wall -Wextra -Werror
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("Hello World!\n");
+    return 0;
+}
